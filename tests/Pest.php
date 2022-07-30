a3d53c6247bb3e221e9e54e822122593f09a730e
Sebastian Fix <sebastian.fix@codebar.ch>
2022-07-30 17:40:21 +0200

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,5 @@
 <?php
 
 use CodebarAg\LaravelDefaultNova\Tests\TestCase;
-use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 
 uses(TestCase::class)->in(__DIR__);
-uses(InteractsWithViews::class)->in('Components');
